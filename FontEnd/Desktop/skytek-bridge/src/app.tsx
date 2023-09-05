3537f5799ff8a1d94f2ffd8026685cee8178aef4
bailey <bhsostek@wpi.edu>
2023-09-05 17:44:12 -0400

    Start of route refactor

--- FontEnd/Desktop/skytek-bridge/src/app.tsx
+++ FontEnd/Desktop/skytek-bridge/src/app.tsx
@@ -5,6 +5,14 @@ import { getDevices } from './api/Client';
 import DeviceList from './components/DeviceList';
 import SkyTekMap from './components/SkyTekMap';
 
+// Import our rotuer here
+import {
+  createHashRouter,
+  RouterProvider,
+} from "react-router-dom";
+import React from 'react';
+import AvailableDevices from './routes/AvailableDevices';
+
 // Indicate that the app has loaded
 const { ipcRenderer } = window.require('electron');
 ipcRenderer.invoke("/onLoad").then((result : any) => {
@@ -13,20 +21,41 @@ ipcRenderer.invoke("/onLoad").then((result : any) => {
   console.log("Error", error);
 })
 
-function render() {
-  ReactDOM.render(<div className="App">
-  <Button variant="contained" onClick={() => {
-    getDevices().then((data) => {
-      console.log("Data", data);
-    }).catch((err) => {
-      console.log("Caught Error", err);
-    });
-  }}>
-    Query Connected Devices
-  </Button>
-  <DeviceList message='Test A Roo'/>
-  <SkyTekMap height={200}></SkyTekMap>
-</div>, document.body);
-}
+// Define our rotuer
+const router = createHashRouter([
+  {
+    path: "/",
+    element: <div>Hello world!</div>,
+    children:[
+      {
+        path:"devices/",
+        element: <AvailableDevices/>
+      }
+    ]
+  },
+]);
 
-render();
+// Define our router
+ReactDOM.render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+, document.body);
+
+// function render() {
+//   ReactDOM.render(<div className="App">
+//   <Button variant="contained" onClick={() => {
+//     getDevices().then((data) => {
+//       console.log("Data", data);
+//     }).catch((err) => {
+//       console.log("Caught Error", err);
+//     });
+//   }}>
+//     Query Connected Devices
+//   </Button>
+//   <DeviceList message='Test A Roo'/>
+//   <SkyTekMap height={200}></SkyTekMap>
+// </div>, document.body);
+// }
+
+// render();
---
+++ FontEnd/Desktop/skytek-bridge/src/routes/AvailableDevices.tsx
@@ -0,0 +1,60 @@
+import React, { useState } from 'react';
+
+// Material UI
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+import IconButton from '@mui/material/IconButton';
+import MenuIcon from '@mui/icons-material/Menu';
+import { styled } from '@mui/material/styles';
+
+// Import our store
+import { useStore } from 'zustand'
+import { useDeviceStore } from '../api/store/DeviceStore';
+
+
+// API
+import { query } from '../api/Client';
+
+// Types
+interface Props {
+  
+}
+
+const AvailableDevices = ({}: Props) => {
+
+  // Here is the Zustand store of our devices.
+  const deviceStore = useStore(useDeviceStore);
+
+  // Theme Info
+  const Demo = styled('div')(({ theme }) => ({
+    backgroundColor: theme.palette.background.paper,
+  }));
+
+
+  return (
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar position="static">
+        <Toolbar>
+          <IconButton
+            size="large"
+            edge="start"
+            color="inherit"
+            aria-label="menu"
+            sx={{ mr: 2 }}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+            News
+          </Typography>
+          <Button color="inherit">Login</Button>
+        </Toolbar>
+      </AppBar>
+    </Box>
+  );
+};
+
+export default AvailableDevices;
